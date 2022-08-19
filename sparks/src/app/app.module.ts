c023614c27868890a16c8c3c9bb3d31a57c8a2ad
Mdsparks17 <56485515+Mdsparks17@users.noreply.github.com>
2022-08-19 16:53:41 -0400

    first commit

---
+++ sparks/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ArticlePageComponent } from './article-page/article-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    ToolbarComponent,
+    ArticlePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
